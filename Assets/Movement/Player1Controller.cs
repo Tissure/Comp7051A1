17a2d3cdbd6a7fbdfcc6890533127c1f2599913d
Jay Wang <jwang550@my.bcit.ca>
2023-09-23 21:38:06 -0700

    Player 1,2 Paddle Prefab Done

---
+++ Assets/Movement/Player1Controller.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player1Controller : MonoBehaviour
+{
+    // Create Private Internal References
+    private InputActionsP1 inputActions;
+    private InputAction movement;
+
+    [SerializeField]
+    public float movementSpeed = 5.0f;
+
+    private void Awake()
+    {
+        // Create new InputActions
+        inputActions = new InputActionsP1();
+
+        movement = inputActions.Player1.Movement;
+    }
+
+    // Called when Script is Enabled
+    private void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // Extract 2D Input Data
+        Vector2 v2 = movement.ReadValue<Vector2>();
+
+        transform.Translate(new Vector3(v2.y, v2.x, 0) * movementSpeed * Time.deltaTime);
+
+    }
+}
