0ac1dc95d4fcfd63fb8410df8d7a6fbfca7e0764
Kyle <kyle.ng7512@gmail.com>
2023-09-26 20:47:45 -0700

    AI added

---
+++ Assets/Movement/AIController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AIController : MonoBehaviour
+{
+    [SerializeField]
+    public float movementSpeed = 5.0f;
+
+    private Transform ballTransform;
+    private void Start()
+    {
+        ballTransform = GameObject.Find("Ball(Clone)").GetComponent<Transform>();
+    }
+    private void FixedUpdate()
+    {
+        Vector2 v2;
+        _ = (transform.position.z > ballTransform.position.z) ? (v2 = Vector2.down) : (v2 = Vector2.up);
+
+        transform.Translate(new Vector3(v2.y, v2.x, 0) * movementSpeed * Time.deltaTime);
+
+
+    }
+}
